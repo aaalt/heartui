d73f6e7e313577092ac680f0ba1f83758488cd79
altnova <arina@lateralsummer.com>
2018-10-08 00:29:53 +0300

    [~]

--- src/draw.c
+++ src/draw.c
@@ -24,8 +24,7 @@ V draw_side(S col, C ch, I w, I dif)
 	fflush(stdout);
 }
 
-
-V show_img(background heart_par, ter_conf _ter_conf)								//< print img 
+V show_img(background heart_par, ter_conf _ter_conf)										//< print img 
 {
 	I j, dif, k = 0, height = _ter_conf->ws_row;
 	struct winsize a;
@@ -97,8 +96,6 @@ V show_img(background heart_par, ter_conf _ter_conf)								//< print img
 	DO((_ter_conf->ws_row - HEART_H)/2 + dif, {DO(_ter_conf->ws_col, {O("%c", heart_par->ch);}); O("\n");}); 	//< draw down
 	// DO((_ter_conf->ws_row - HEART_H)/2 + dif - 3, {DO(_ter_conf->ws_col, {O("%c", heart_par->ch);}); O("\n");}); 	//< draw down (test mode)
 
-	// O("w %d * h %d\n", _ter_conf->ws_col, _ter_conf->ws_row);
-
 	fflush(stdout);
 }
 
@@ -111,7 +108,6 @@ V window_dif_change(background heart_par, ter_conf _ter_conf, I pause)
 	j = pause/p;
 
 	show_img(heart_par, _ter_conf);
-	// O("w %d * h %d\n", _ter_conf->ws_col, _ter_conf->ws_row);
 	for (i = 0; i < j; i++) {
 		usleep(p);
 		ioctl(STDOUT_FILENO, TIOCGWINSZ, _ter_var);
@@ -123,7 +119,8 @@ V window_dif_change(background heart_par, ter_conf _ter_conf, I pause)
 
 V draw_heart(S* col_palette, S* blink_st_palette, S* blink_col_palette, S* str_st_palette, S* str_col_palette)										//< print rand img
 {
-	I pos, i, j, pause = SEC, n, num = 0;
+	I pause = SEC, n, num = 0;
+	I len = lines();
 
 	pParams para = {0};
 	params par = &para;
@@ -142,7 +139,7 @@ V draw_heart(S* col_palette, S* blink_st_palette, S* blink_col_palette, S* str_s
 
 	CLEAR;
 	for (;;) {
-		make_struct_nmb(nmb);
+		make_struct_nmb(nmb, len);
 
 		num_to_back(nmb, heart_par, col_palette, blink_st_palette, blink_col_palette, str_st_palette, str_col_palette);
 		
@@ -161,7 +158,4 @@ V draw_heart(S* col_palette, S* blink_st_palette, S* blink_col_palette, S* str_s
 	free_array(blink_col_palette, 2);
 	free_array(str_st_palette, 5);
 	free_array(str_col_palette, 3);
-
-/*	fclose(db);
-	fclose(idx);*/
 }
