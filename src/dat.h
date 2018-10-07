6025a4e5a2c7de7fe42b32669b09b14ba237d5b4
altnova <arina@lateralsummer.com>
2018-10-08 00:29:31 +0300

    len as param in make_struct_nmb

--- src/dat.h
+++ src/dat.h
@@ -7,7 +7,7 @@ ext C blstr_mask(C blink, C str);
 
 ext C get_blink(C ch);
 ext	C get_str(C ch);
-ext	V make_struct_nmb(numbers par);
+ext	V make_struct_nmb(numbers par, I len);
 
 
 ext	V num_to_back(numbers par, background back, S* col_palette, S* blink_st_palette, S* blink_col_palette, S* str_st_palette, S* str_col_palette);
